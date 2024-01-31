6e3571c1a4b93148a6b8e866b8e5c57d96144ecd
Charles Coleman <bigglesandginger@gmail.com>
2024-01-31 09:33:59 -0500

    add environment variables

--- api/settings.py
+++ api/settings.py
@@ -5,18 +5,19 @@ import os
 
 
 class Settings(BaseSettings):
-    esgf_url: str = Field("https://esgf-node.llnl.gov/esg-search")
+    esgf_url: str = Field(os.environ.get("ESGF_URL", "https://esgf-node.llnl.gov/esg-search"))
     default_facets: str = Field("project,experiment_family")
     entries_per_page: int = Field(20)
 
-    redis: Dict[str, Any] = Field({"host": "redis-climate-data", "port": 6379})
+    redis_host: Field(os.environ.get("REDIS_HOST", "redis-climate-data"))
+    redis_port: Field(os.environ.get("REDIS_HOST", 6379))
 
-    minio_url: str = Field("http://minio:9000")
-    minio_user: str = Field("miniouser")
-    minio_pass: str = Field("miniopass")
-    bucket_name: str = Field("climate-data-test-bucket")
+    minio_url: str = Field(os.environ.get("MINIO_URL", "http://minio:9000"))
+    minio_user: str = Field(os.environ.get("MINIO_USER", "miniouser"))
+    minio_pass: str = Field(os.environ.get("MINIO_PASS", "miniopass"))
+    bucket_name: str = Field(os.environ.get("MINIO_BUCKET_NAME", "climate-data-test-bucket"))
 
-    terarium_url: str = Field("https://server.staging.terarium.ai")
+    terarium_url: str = Field(os.environ.get("TERARIUM_URL", "https://server.staging.terarium.ai"))
     terarium_user: str = Field(os.environ.get("TERARIUM_USER", ""))
     terarium_pass: str = Field(os.environ.get("TERARIUM_PASS", ""))
 
